5d257ba20e48367ea216ec36e246b4d4b1e62ce4
Davi Marcondes Moreira <davi.marcondes.moreira@gmail.com>
2014-12-02 00:31:13 -0200

    Class itself. On development.

---
+++ src/DevGears/Validator/PasswordStrength.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace DevGears\Validator;
+
+use Zend\Validator\AbstractValidator;
+
+/**
+ * PasswordStrength
+ *
+ * A password strength validator, designed for Zend Framework 2.* projects.
+ *
+ * @link https://github.com/DevGears/ZF2-PasswordStrengthValidator
+ * @author Davi Marcondes Moreira (@devdrops) <davi.marcondes.moreira@gmail.com>
+ */
+class PasswordStrength extends AbstractValidator
+{
+    
+}
